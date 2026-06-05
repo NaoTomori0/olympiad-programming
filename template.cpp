3aa683d2220cc951df9b625cc08cbdd36556e05d
NaoTomori0 <kyongfam0@gmail.com>
2026-06-06 02:11:51 +0300

    add template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> // Подключает сразу все библиотеки C++
+using namespace std;
+
+#define int long long // Защита от переполнения int (из-за этого часто горят баллы)
+
+void solve()
+{
+    // Твой код здесь
+}
+
+int32_t main()
+{
+    // Включаем супер-быстрый ввод-вывод для C++
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return 0;
+}
